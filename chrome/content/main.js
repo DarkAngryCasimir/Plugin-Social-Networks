0a2a4b292f1d785a051a98ba01806d6b9dc08c82
Domingues Nicolas <domingues.nicolas@gmail.com>
2012-02-20 13:58:03 +0100

    update 20/02
    Signed-off-by: Dark_Angry_Casimir

--- chrome/content/main.js
+++ chrome/content/main.js
@@ -1,13 +1,13 @@
 
 // Make a namespace.
-if (typeof SocialNetworksPlugin == 'undefined') {
-  var SocialNetworksPlugin = {};
+if (typeof Socialnetworks == 'undefined') {
+  var Socialnetworks = {};
 }
 
 /**
  * UI controller that is loaded into the main player window
  */
-SocialNetworksPlugin.Controller = {
+Socialnetworks.Controller = {
 
   /**
    * Called when the window finishes loading
@@ -20,30 +20,17 @@ SocialNetworksPlugin.Controller = {
 
     // initialization code
     this._initialized = true;
-    this._strings = document.getElementById("social-networks-plugin-strings");
+    this._strings = document.getElementById("socialnetworks-strings");
     
+	
     // Perform extra actions the first time the extension is run
-    if (Application.prefs.get("extensions.social-networks-plugin.firstrun").value) {
-      Application.prefs.setValue("extensions.social-networks-plugin.firstrun", false);
+    if (Application.prefs.get("extensions.socialnetworks.firstrun").value) {
+      Application.prefs.setValue("extensions.socialnetworks.firstrun", false);
       this._firstRunSetup();
     }
 
 
-    // Add the toolbar button to the default item set of the browser toolbar.
-    // TODO: Should only do this on first run, but Bug 6778 requires doing it
-    // every load.
-    this._insertToolbarItem("nav-bar", "social-networks-plugin-toolbarbutton", "subscription-button");
-
     
-
-    // Make a local variable for this controller so that
-    // it is easy to access from closures.
-    var controller = this;
-    
-    // Attach doHelloWorld to our helloworld command
-    this._helloWorldCmd = document.getElementById("social-networks-plugin-helloworld-cmd");
-    this._helloWorldCmd.addEventListener("command", 
-         function() { controller.doHelloWorld(); }, false);
 
   },
   
@@ -56,14 +43,6 @@ SocialNetworksPlugin.Controller = {
   },
   
 
-  /**
-   * Sample command action
-   */
-  doHelloWorld : function() {
-    var message = "SocialNetworksPlugin: " + this._strings.getString("helloMessage");
-    alert(message);
-  },
-
   
   /**
    * Perform extra setup the first time the extension is run
@@ -71,7 +50,7 @@ SocialNetworksPlugin.Controller = {
   _firstRunSetup : function() {
   
     // Call this.doHelloWorld() after a 3 second timeout
-    setTimeout(function(controller) { controller.doHelloWorld(); }, 3000, this); 
+    alert("premier lancement");
   
   },
 
@@ -97,38 +76,21 @@ SocialNetworksPlugin.Controller = {
       servicesNode.hidden = false;
     }
 
-	
-	var radioNode = this._servicePaneService.getNode('SB:RadioStations');
-    if (!radioNode) {
-      radioNode = this._servicePaneService.createNode();
-      radioNode.id = 'SB:RadioStations';
-      radioNode.className = 'folder radio';
-      radioNode.editable = false;
-      radioNode.name = SBString("servicesource.radio");
-      radioNode.setAttributeNS('http://getnightingale.com/rdf/servicepane#',
-                               'Weight',
-                               1);
-      this._servicePaneService.root.appendChild(radioNode);
-    } else {
-      radioNode.hidden = false;
-    }
      
-	var addonNode = this._servicePaneService.getNode('SB:SocialNetworksPlugin');
+	var addonNode = this._servicePaneService.getNode('SB:Socialnetworks');
 	if (!addonNode) {
 		var myNode = this._servicePaneService.createNode();
-		myNode.id = 'SB:SocialNetworksPlugin';
-		myNode.url = 'chrome://social-networks-plugin/content/mainview.xul';
-		myNode.image = 'chrome://social-networks-plugin/skin/node-icon.png';
-		myNode.className = 'SocialNetworksPlugin';
-		myNode.name = 'Social Networks plugin';
+		myNode.id = 'SB:Socialnetworks';
+		myNode.url = 'chrome://socialnetworks/content/mainview.xul';
+		myNode.image = 'chrome://socialnetworks/skin/node-icon.png';
+		myNode.className = 'Socialnetworks';
+		myNode.name = 'Social-Networks';
 		myNode.setAttributeNS('http://getnightingale.com/rdf/servicepane#',
                           "addonID",
-                          "{b359fb9e-293e-4bc7-af6f-388ae1201aaa}");
+                          "{cc4721c3-4a77-4462-a8cf-e9ec6939452e}");
 
 		servicesNode.appendChild(myNode);
 
-
-		radioNode.appendChild(myNode);
 
   }
   else {
@@ -141,39 +103,8 @@ SocialNetworksPlugin.Controller = {
   
 
 
-  /**
-   * Helper to add a toolbaritem within a given toolbar
-   * 
-   *   toolbar - the ID of a toolbar element
-   *   newItem - the ID of a toolbaritem element within the 
-   *            associated toolbarpalette
-   *   insertAfter - ID of an toolbaritem after which newItem should appear
-   */
-  _insertToolbarItem: function(toolbar, newItem, insertAfter) {
-    var toolbar = document.getElementById(toolbar);
-    var list = toolbar.currentSet || "";
-    list = list.split(",");
-    
-    // If this item is not already in the current set, add it
-    if (list.indexOf(newItem) == -1)
-    {
-      // Add to the array, then recombine
-      insertAfter = list.indexOf(insertAfter);
-      if (insertAfter == -1) {
-        list.push(newItem);
-      } else {
-        list.splice(insertAfter + 1, 0, newItem);
-      }
-      list = list.join(",");
-      
-      toolbar.setAttribute("currentset", list);
-      toolbar.currentSet = list;
-      document.persist(toolbar.id, "currentset");
-    }
-  }
-
   
 };
 
-window.addEventListener("load", function(e) { SocialNetworksPlugin.Controller.onLoad(e); }, false);
-window.addEventListener("unload", function(e) { SocialNetworksPlugin.Controller.onUnLoad(e); }, false);
+window.addEventListener("load", function(e) { Socialnetworks.Controller.onLoad(e); }, false);
+window.addEventListener("unload", function(e) { Socialnetworks.Controller.onUnLoad(e); }, false);
--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,11 +1,7 @@
 
-pref("extensions.social-networks-plugin.boolpref", false);
-pref("extensions.social-networks-plugin.intpref", 0);
-pref("extensions.social-networks-plugin.stringpref", "A string");
-
 
 // Used to detect the first time the extension is run 
-pref("extensions.social-networks-plugin.firstrun", true);
+pref("extensions.socialnetworks.firstrun", true);
 
 // See http://kb.mozillazine.org/Localize_extension_descriptions
-// pref("extensions.{b359fb9e-293e-4bc7-af6f-388ae1201aaa}.description", "chrome://social-networks-plugin/locale/social-networks-plugin.properties");
+// pref("extensions.{cc4721c3-4a77-4462-a8cf-e9ec6939452e}.description", "chrome://socialnetworks/locale/socialnetworks.properties");
