f26ca6eeadadbf8ce1866fbee0ea81f82e88e81b
Domingues Nicolas <domingues.nicolas@gmail.com>
2012-03-05 09:07:55 +0100

    Signed-off-by: Dark_Angry_Casimir

--- chrome/content/Login.js
+++ chrome/content/Login.js
@@ -1,3 +1,11 @@
 function openLoginTwitter() {
-	window.openDialog("chrome://plugin-social-networks-for-nightingale/content/twitLog.xul", "_blank", "chrome,modal=no,titlebar=no,resizable=no", null);
+	//window.openDialog("chrome://plugin-social-networks-for-nightingale/content/twitLog.xul", "_blank", "chrome,modal=no,titlebar=no,resizable=no", null);
+	var windowWidth = 500;
+	var windowHeight = 200;
+	
+	window.openDialog('twitLog2.xul','Connection_to_Twitter','left='+((window.outerWidth/2)-(windowWidth/2))+','
+                                                + 'top='+((window.outerHeight/2)-(windowHeight/2))+','
+												+ 'width=' + windowWidth + ','
+												+ 'height=' + windowHeight + ','
+												+ 'chrome,modal=no,titlebar=no,resizable=no', null);
 }
