0618ceccb9220f8a3bb052448002c23faf17380b
Domingues Nicolas <domingues.nicolas@gmail.com>
2012-03-05 09:14:26 +0100

    v1.1 : interface terminée
    Signed-off-by: Dark_Angry_Casimir

--- chrome/content/Login.js
+++ chrome/content/Login.js
@@ -1,9 +1,8 @@
 function openLoginTwitter() {
-	//window.openDialog("chrome://plugin-social-networks-for-nightingale/content/twitLog.xul", "_blank", "chrome,modal=no,titlebar=no,resizable=no", null);
 	var windowWidth = 500;
 	var windowHeight = 200;
 	
-	window.openDialog('twitLog2.xul','Connection_to_Twitter','left='+((window.outerWidth/2)-(windowWidth/2))+','
+	window.openDialog('twitLog.xul','Connection_to_Twitter','left='+((window.outerWidth/2)-(windowWidth/2))+','
                                                 + 'top='+((window.outerHeight/2)-(windowHeight/2))+','
 												+ 'width=' + windowWidth + ','
 												+ 'height=' + windowHeight + ','
