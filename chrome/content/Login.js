b4d1394939b9e4a1071d11b37231573a647c218d
Domingues Nicolas <domingues.nicolas@gmail.com>
2012-03-11 17:07:17 +0100

    Commit final : prémices de l'OAuth Twitter
    Signed-off-by: Dark_Angry_Casimir

--- chrome/content/Login.js
+++ chrome/content/Login.js
@@ -1,3 +1,5 @@
+
+
 function openLoginTwitter() {
 	var windowWidth = 500;
 	var windowHeight = 200;
@@ -7,4 +9,19 @@ function openLoginTwitter() {
 												+ 'width=' + windowWidth + ','
 												+ 'height=' + windowHeight + ','
 												+ 'chrome,modal=no,titlebar=no,resizable=no', null);
+}
+
+function openLoginFacebook() {
+	var windowWidth = 500;
+	var windowHeight = 200;
+	
+	window.openDialog('fbLog.xul','Connection_to_Facebook','left='+((window.outerWidth/2)-(windowWidth/2))+','
+                                                + 'top='+((window.outerHeight/2)-(windowHeight/2))+','
+												+ 'width=' + windowWidth + ','
+												+ 'height=' + windowHeight + ','
+												+ 'chrome,modal=no,titlebar=no,resizable=no', null);
+}
+
+function connectTwitter(mavar, mavar2){
+	//window.openDialog('browser.xul','Twitter','chrome,modal=no,titlebar=yes,resizable=yes', mavar, mavar2);
 }
